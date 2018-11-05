1e8753207e29ec0cf7173e3b7c9db6359d992942
Heidi Engstrom <hengstrom25@gmail.com>
2018-11-05 21:27:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
     console.log('Welcome ${names[i]}! You are employee #$[i+1}')
   }
-  return names
+  return names;
 }
 
 
