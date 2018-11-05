0253d58c5e920bff0fc69f0f9c9cbd2932a129bc
Heidi Engstrom <hengstrom25@gmail.com>
2018-11-05 21:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+fuction tailsNeverFails() {
+    return Math.random() >= 0.5;
+}
+ 
+while (tailsNeverFails()) {
+    console.log('You got #{} tails in a row!);
+}
