33cf8377cd17afd49151952eb9c9ceb60e85be63
Heidi Engstrom <hengstrom25@gmail.com>
2018-11-05 21:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,4 @@ function printBadges(names) {
   }
 }
 
-function tailsNeverFails() {
-    return Math.random() >= 0.5;
-}
- 
-while (tailsNeverFails()) {
-    console.log('You got #{} tails in a row!);
-}
+
